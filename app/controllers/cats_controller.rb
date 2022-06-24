579c33c390e08c133cf821bb67f5624bf83e9b8e
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-06-24 15:58:41 +0900

    見た目を整理

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,16 +3,9 @@ class CatsController < ApplicationController
 
   # GET /cats
   def index
-  # `Cat.ransack`でCatに対してransackを使う
-   # params[:q]には検索フォームで指定した検索条件が入る
-   @search = Cat.ransack(params[:q])
-
-   # デフォルトのソートをid降順にする
-   @search.sorts = 'id desc' if @search.sorts.empty?
-
-   # `@search.result`で検索結果となる@catsを取得する
-   # 検索結果に対してはkaminariのpageメソッドをチェーンできる
-   @cats = @search.result.page(params[:page])
+    @search = Cat.ransack(params[:q])
+    @search.sorts = 'id desc' if @search.sorts.empty?
+    @cats = @search.result.page(params[:page])
   end
 
   # GET /cats/1
@@ -33,7 +26,7 @@ class CatsController < ApplicationController
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to @cat, notice: "Cat was successfully created."
+      redirect_to @cat, notice: "ねこを登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
@@ -42,7 +35,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "Cat was successfully updated."
+      redirect_to @cat, notice: "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -51,7 +44,7 @@ class CatsController < ApplicationController
   # DELETE /cats/1
   def destroy
     @cat.destroy
-    redirect_to cats_url, notice: "Cat was successfully destroyed."
+    redirect_to cats_url, notice: "ねこを削除しました。"
   end
 
   private
